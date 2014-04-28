5eb9a87fceb1ca2f39b7fcb80271f13acf012cea
Darrick Wiebe <dw@xnlogic.com>
2014-04-28 12:27:56 -0400

    Switch to jbundler now that I can get it to correctly pull in Kafka

--- lib/jruby-kafka.rb
+++ lib/jruby-kafka.rb
@@ -1,14 +1,12 @@
 # Because of a problem with a kafka dependency, jbundler 0.5.5 does not work. Therefore, you
 # need to do one of the following to have your Kafka jar dependencies available:
 #
+# - run your app under jbundler (adds a layer to bundler to manage jar dependencies)
 # - already have the Kafka jars loaded before requiring jruby-kafka
 # - set KAFKA_PATH in the environment to point to a Kafka binary installation
-# - run 'lein uberjar' to build the standalone jar
 #
-if ENV['KAFKA_PATH']
+if not JBUNDLER_CLASSPATH and ENV['KAFKA_PATH']
   require 'jruby-kafka/loader'
-else
-  require "target/jruby-kafka-jar-standalone.jar" rescue nil
 end
 
 require "jruby-kafka/consumer"
