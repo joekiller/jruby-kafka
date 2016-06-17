b53bf3ecc7e3af5a3d4e2c331a85f204eb87317a
Elias Levy <fearsome.lucidity@gmail.com>
2016-06-16 19:40:05 -0700

    Simplify KafkaProducer
    - Make it inherit from Java's KafkaProducer, to match Kafka::KafkaConsumer.
    - Rename options attribute to properties to match Kafka::KafkaConsumer.
    - Remove required property check.  Java's KafkaProducer already performs the check.

--- lib/jruby-kafka/kafka-producer.rb
+++ lib/jruby-kafka/kafka-producer.rb
@@ -1,16 +1,10 @@
 require 'jruby-kafka/namespace'
-require 'jruby-kafka/error'
 require 'jruby-kafka/utility'
 
 # noinspection JRubyStringImportInspection
-class Kafka::KafkaProducer
+class Kafka::KafkaProducer <  Java::org.apache.kafka.clients.producer.KafkaProducer
   java_import 'org.apache.kafka.clients.producer.ProducerRecord'
   java_import 'org.apache.kafka.clients.producer.Callback'
-  KAFKA_PRODUCER = Java::org.apache.kafka.clients.producer.KafkaProducer
-
-  REQUIRED = [
-    :bootstrap_servers, :key_serializer
-  ]
 
   class RubyCallback
     include Callback
@@ -24,30 +18,34 @@ class Kafka::KafkaProducer
     end
   end
 
-  attr_reader :producer, :send_method, :send_cb_method, :options
+  attr_reader :properties
 
+  # Create a Kafka producer.
+  #
+  # @param [Hash] config the producer configuration.
+  #
+  # @option config [String]  :bootstrap_servers A list of host/port pairs to use for establishing the initial connection to the Kafka cluster. Required.
+  # @option config [String]  :key_serializer Serializer class for key that implements the Deserializer interface. Required.
+  # @option config [String]  :value_serializer Serializer class for value that implements the Deserializer interface. Required.
+  #
+  # For other configuration properties and their default values see 
+  # http://kafka.apache.org/documentation.html#producerconfigs and
+  # http://kafka.apache.org/0100/javadoc/index.html?org/apache/kafka/clients/consumer/KafkaConsumer.html.
+  #
   def initialize(opts = {})
-    Kafka::Utility.validate_arguments REQUIRED, opts
-    @options = opts
-    @send_method = @send_cb_method = proc { throw StandardError.new 'Producer is not connected' }
+    @properties = opts.clone
+    super Kafka::Utility.java_properties @properties
   end
 
-  def connect
-    @producer = KAFKA_PRODUCER.new(Kafka::Utility.java_properties @options)
-    @send_method = producer.java_method :send, [ProducerRecord]
-    @send_cb_method = producer.java_method :send, [ProducerRecord, Callback.java_class]
-  end
+  java_alias :send_method   , :send, [ProducerRecord]
+  java_alias :send_cb_method, :send, [ProducerRecord, Callback.java_class]
 
   # throws FailedToSendMessageException or if not connected, StandardError.
   def send_msg(topic, partition, key, value, &block)
     if block
-      send_cb_method.call(ProducerRecord.new(topic, partition, key, value), RubyCallback.new(block))
+      send_cb_method ProducerRecord.new(topic, partition, key, value), RubyCallback.new(block)
     else
-      send_method.call(ProducerRecord.new(topic, partition, key, value))
+      send_method ProducerRecord.new(topic, partition, key, value)
     end
-  end
-
-  def close
-    @producer.close
   end
 end
